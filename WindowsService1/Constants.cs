45898c1c26bc72d323da103ae2b68ebe6a81018d
Sherif Elmetainy <sherif.elmetainy@code-art.net>
2018-03-21 22:23:22 +0200

    Add project files.

---
+++ WindowsService1/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowsService1
+{
+	internal static class Constants
+	{
+		public const string ServiceName = "MyService";
+		public const string ServiceDisplayName = "My Service";
+		public const string ServiceDescription = "My Service Description";
+	}
+}
---
+++ WindowsService1/Service1Installer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace WindowsService1
+{
+	[RunInstaller(true)]
+	public class Service1Installer : Installer
+	{
+		public Service1Installer()
+		{
+			var serviceProcessInstaller = new ServiceProcessInstaller
+			{
+				Account = ServiceAccount.LocalSystem
+			};
+			var serviceInstaller = new ServiceInstaller
+			{
+				Description = Constants.ServiceDescription,
+				DisplayName = Constants.ServiceDisplayName,
+				ServiceName = Constants.ServiceName,
+				StartType = ServiceStartMode.Automatic
+			};
+			Installers.Add(serviceProcessInstaller);
+			Installers.Add(serviceInstaller);
+		}
+
+		protected override void OnBeforeUninstall(IDictionary savedState)
+		{
+			try
+			{
+				var serviceController = new ServiceController(Constants.ServiceName);
+				if (serviceController.Status == ServiceControllerStatus.Stopped)
+				{
+					serviceController.Stop();
+					serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+				}
+			}
+			catch
+			{
+				// ignored
+			}
+			base.OnBeforeUninstall(savedState);
+		}
+
+		protected override void OnAfterInstall(IDictionary savedState)
+		{
+			base.OnAfterInstall(savedState);
+			try
+			{
+				var serviceController = new ServiceController(Constants.ServiceName);
+				serviceController.Start();
+			}
+			catch
+			{
+				// ignored
+			}
+		}
+	}
+}
